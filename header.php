fc79ff62c917e0ec613f81a24b176c711f3fb734
Yash Gandhi <yashgandhi2020@gmail.com>
2019-11-14 02:01:49 +0530

    added header and all stuff done

---
+++ header.php
@@ -0,0 +1,76 @@
+<!Doctype html>
+<html lang="en" class="no-js">
+		<head>
+				<meta charset="utf-8">
+				<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="#fff">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+				<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800|Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+				<link href="https://dscmescoe.com/css/material-kit.css?v=2.1.2" rel="stylesheet" />
+				<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+				<link href="https://dscmescoe.com/css/custom.css" rel="stylesheet" />
+		</head>
+		<nav class="navbar navbar-color-on-scroll navbar-transparent    fixed-top  navbar-expand-lg " color-on-scroll="20" id="sectionsNav">
+				<div class="container">
+						<div class="navbar-translate">
+								<div class="navbar-brand logo">
+										<div class="logo-big">
+												<a href="https://dscmescoe.com" style="color: inherit;"><img src="https://dscmescoe.com/images/logo.png" class="img-fluid" style="margin-bottom: 5px !important;"></a>
+												<span><a href="https://dscmescoe.com" style="color: inherit;"> DSC MESCOE </a></span>
+										</div>	
+										<div class="logo-small">
+												<a href="https://dscmescoe.com" style="color: inherit;"><img src="https://dscmescoe.com/images/logo.png" class="img-fluid" style="margin-bottom: 5px !important;"></a>
+												<span><a href="https://dscmescoe.com" style="color: inherit;"> DSC MESCOE </a></span>
+										</div>
+								</div>
+								<a href="https://flutter.dev/">
+									<div class="navbar-toggler">
+										<span class=""><img src="https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg" class="img-fluid"></span>
+									</div>
+								</a>
+						</div>
+						<div class="navbar-translate">
+							<button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="navbar-toggler-icon"></span>
+									<span class="navbar-toggler-icon"></span>
+									<span class="navbar-toggler-icon"></span>
+							</button>
+						</div>
+						<div class="collapse navbar-collapse">
+								<ul class="main-page navbar-nav ml-auto">
+										<li class="nav-item">
+												<a href="#AboutUs" class="nav-link">
+												Schedule
+												</a>
+										</li>
+										<li class="nav-item">
+												<a href="#Register" class="nav-link">
+												Attending
+												</a>
+										</li>
+										<li class="nav-item">
+												<a href="#Register" class="nav-link">
+												Guidlines
+												</a>
+										</li>
+										<li class="nav-item">
+											<a href="https://flutter.dev/" style="padding-left: 50px"><img src="https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg" class="img-fluid"></a>
+										</li>
+								</ul>
+						</div>					
+				</div>
+				
+		</nav>
+		<script src="https://dscmescoe.com/js/core/jquery.min.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://dscmescoe.com/js/core/popper.min.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://dscmescoe.com/js/core/bootstrap-material-design.min.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://dscmescoe.com/js/plugins/moment.min.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://dscmescoe.com/js/material-kit.js?v=2.1.1" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://dscmescoe.com/js/custom.js" type="e9f3a9731ad1e7ab4b8b795a-text/javascript"></script>
+		<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/95c75768/cloudflare-static/rocket-loader.min.js" data-cf-settings="e9f3a9731ad1e7ab4b8b795a-|49" defer=""></script></body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,512 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+<meta name="google-signin-client-id" content="721724668570-nbkv1cfusk7kk4eni4pjvepaus73b13t.apps.googleusercontent.com">
+<meta name="google-signin-scope" content="profile email">
+<meta property="og:site_name" content="Google Developers">
+<meta property="og:type" content="website">
+<meta name="theme-color" content="#1a73e8">
+<meta charset="utf-8">
+<meta content="IE=Edge" http-equiv="X-UA-Compatible">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Google+Sans:400,500|Roboto:400,400italic,500,500italic,700,700italic|Roboto+Mono:400,500,700|Material+Icons">
+<link rel="stylesheet" href="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/css/app.css">
+<noscript>
+<link rel="stylesheet" href="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/css/ce_bundle.css">
+</noscript>
+<link rel="shortcut icon" href="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/images/favicon.png">
+<link rel="apple-touch-icon" href="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/images/touchicon-180.png"><link rel="canonical" href="https://developers.google.com/events/flutter-interact/attending"><link rel="search" type="application/opensearchdescription+xml"
+title="Google Developers" href="https://developers.google.com/s/opensearch.xml">
+<title>Flutter Interact &nbsp;|&nbsp; Google Developers</title>
+<meta property="og:title" content="Flutter Interact &nbsp;|&nbsp; Google Developers">
+<meta name="description" content="Join us for a day of creation and collaboration with the world, broadcasted live from Brooklyn NY.">
+<meta property="og:description" content="Join us for a day of creation and collaboration with the world, broadcasted live from Brooklyn NY.">
+<meta property="og:url" content="https://developers.google.com/events/flutter-interact/attending">
+<meta property="og:image" content="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/images/opengraph/google-blue.png">
+<meta property="og:image:secure_url" content="https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/images/opengraph/google-blue.png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="675">
+<meta property="og:locale" content="en">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="xsrf_token" content="7yrSDoTvIvXEUJZCNu1BhfSuO4p75KcsOqm_7QVUMeE6MTU3MzY2MTcyNTE3MTg4NQ" />
+</head>
+
+<body type="landing"
+theme="google-blue"
+class="flutter-interact"
+layout="full"
+pendingd>
+
+<?php
+    require "header.php"
+?>
+<div class="devsite-wrapper">
+<div id="gc-wrapper">
+<div class="devsite-main-content">
+<devsite-content>
+
+<article class="devsite-article">
+<article class="devsite-article-inner"><style type="text/css">/* Common event styles */
+@import url("/site-assets/styles/events.css");
+
+h1, h2, h3, h4, h5, h6 {
+font-family: 'Google Sans', sans-serif;
+}
+
+.flutter-interact .fi-section {
+padding: 64px 0 24px;
+}
+
+.flutter-interact .fi-section--hero {
+background-color: #112332;
+background-image: url(/events/flutter-interact/images/hero_texture.svg);
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+color: #fff;
+}
+
+.flutter-interact .fi-section--dark {
+background-color: #112332;
+color: #fff;
+}
+
+.flutter-interact .fi-col h3 {
+font-family: 'Google Sans';
+}
+
+.flutter-interact .fi-col--25 {
+flex: 25%;
+}
+
+.flutter-interact .fi-col--75 {
+flex: 75%;
+}
+
+.flutter-interact .fi-col--40 {
+flex: 40%;
+}
+
+.flutter-interact .fi-col--60 {
+flex: 60%;
+}
+
+.flutter-interact .fi-col--30 {
+flex: 30%;
+}
+
+.flutter-interact .fi-col--70 {
+flex: 70%;
+}
+
+/* DevSite Overrides */
+.flutter-interact .devsite-footer,
+.flutter-interact .devsite-footer .devsite-footer-utility,
+.flutter-interact .devsite-footer .devsite-footer-linkboxes {
+background-color: #F8F9FA;
+}
+
+.flutter-interact
+.devsite-landing-row:not(.devsite-landing-row-4-up)
+.devsite-landing-row-item-no-media:not(:first-child) {
+margin: 0;
+}
+
+.flutter-interact [background=white] :link:not(.button),
+[background=white] :visited:not(.button) {
+color: #1a73e8;
+outline: 0;
+text-decoration: none;
+}
+
+.flutter-interact .devsite-landing-row-header {
+margin-bottom: 0;
+}
+
+/* Schedule styles */
+.flutter-interact .fi-schedule-table {
+padding: 46px;
+}
+
+.flutter-interact .fi-schedule-table table {
+font-size: 18px;
+}
+
+/* DevSite Override for Schedule */
+.flutter-interact .fi-schedule-table
+table.responsive>*>tr:not(.alt) td:first-child,
+table.responsive>tr:not(.alt) td:first-child {
+background: transparent;
+}
+
+.flutter-interact .fi-schedule-table tr {
+border: 0;
+border-bottom: 1px solid #dadce0;
+}
+
+.flutter-interact .fi-schedule-table tr:last-of-type {
+border: 0;
+}
+
+.flutter-interact .fi-schedule-table td {
+padding: 32px 0;
+}
+
+.flutter-interact .fi-schedule-table h3 {
+font-family: 'Google Sans';
+font-size: 20px;
+line-height: 28px;
+font-weight: normal;
+}
+
+.flutter-interact .fi-schedule-table td:first-of-type {
+width: 25%;
+}
+
+.flutter-interact .fi-schedule-table small {
+font-size: 14px;
+}
+
+.flutter-interact .fi-schedule-table p+p {
+color: #4C5257;
+}
+
+.flutter-interact .fi-schedule-table .fi-speaker-name {
+font-size: 16px;
+}
+
+.flutter-interact .fi-schedule-table .fi-schedule-description {
+font-size: 14px;
+}
+
+.flutter-interact .fi-schedule-table-note {
+padding: 2em 0;
+text-align: right;
+}
+
+.flutter-interact .fi-schedule-table-note {
+font-family: 'Google Sans';
+font-size: 12px;
+line-height: 18px;
+letter-spacing: 0.35;
+}
+
+/* Hero styles (video) */
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-media {
+flex-basis: 50%;
+}
+
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-buttons {
+margin-top: 1em;
+}
+
+.flutter-interact .fi-landing-hero .button-arrow {
+color: #fff;
+}
+
+/* Home cards */
+.flutter-interact .fi-cards h2 {
+margin-left: auto;
+margin-right: auto;
+text-align: center;
+width: 82.5%;
+}
+
+.flutter-interact .fi-cards .devsite-landing-row-item {
+border-radius: 0;
+border: none;
+}
+
+.flutter-interact .fi-cards .devsite-landing-row-item-buttons {
+align-items: start;
+flex-flow: column;
+margin-top: 0;
+}
+
+/* Attending styles */
+.flutter-interact .fi-attending-col ul {
+margin: 0;
+padding-left: 20px;
+}
+
+.flutter-interact .fi-attending-col h4 {
+color: #91979C;
+margin: 32px 0 0;
+}
+
+.flutter-interact .fi-attending-col p {
+margin: 0 0 16px;
+}
+
+.flutter-interact .fi-attending-header h2 {
+width: 100%;
+}
+
+@media (min-width: 1440px) {
+.flutter-interact .fi-section--hero {
+background-size: auto;
+}
+}
+
+/* Speakers styles */
+.flutter-interact .fi-speakers h4 {
+color: #91979C;
+}
+
+
+/* Buttons */
+.flutter-interact .button-arrow {
+background: transparent;
+border: none;
+height: auto;
+line-height: 1.35;
+padding: 4px 8px 5px;
+}
+
+.flutter-interact .button-arrow:after {
+color: inherit;
+content: '\2192';
+display: inline-block;
+margin-left: 6px;
+transform: translateY(1px);
+}
+
+.flutter-interact .button-arrow:focus {
+text-decoration: underline;
+}
+
+/* Heading one */
+.fi-heading-one,
+.flutter-interact .fi-landing-hero h3 {
+font-size: 32px;
+line-height: 72px;
+margin-bottom: .35em;
+}
+
+/* Heading two */
+.fi-heading-two,
+.flutter-interact .fi-section--hero h2,
+.flutter-interact .fi-get-involved h2 {
+font-size: 38px;
+line-height: 1.2;
+margin: 0 0 .25em;
+}
+
+/* Heading description */
+.fi-heading-description,
+.flutter-interact .fi-section--hero .devsite-landing-row-description,
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-description-content {
+font-family: 'Google Sans', sans-serif;
+font-size: 16px;
+line-height: 32px;
+}
+
+/* Media queries */
+@media (min-width: 350px) {
+.flutter-interact .fi-section {
+padding: 40px 0 24px;
+}
+}
+
+@media (min-width: 600px) {
+/* Heading one */
+.fi-heading-one,
+.flutter-interact .fi-landing-hero h3 {
+font-size: 38px;
+letter-spacing: 0;
+}
+
+/* Heading two */
+.fi-heading-two,
+.flutter-interact .fi-section--hero h2,
+.flutter-interact .fi-get-involved h2 {
+font-size: 45px;
+}
+
+/* Heading description */
+.fi-heading-description,
+.flutter-interact .fi-section--hero .devsite-landing-row-description,
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-description-content {
+font-size: 16px;
+letter-spacing: 0;
+}
+
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-description {
+padding-right: 100px;
+}
+
+.flutter-interact .fi-get-involved .devsite-landing-row-column {
+align-self: center;
+margin-left: 1em;
+margin-right: 1em;
+}
+
+.flutter-interact .fi-attending-header h2 {
+width: 90%;
+}
+
+.flutter-interact .fi-section {
+padding: 80px 0 40px;
+}
+
+.flutter-interact .fi-schedule-table h3 {
+font-size: 24px;
+line-height: 32px;
+}
+}
+
+@media (min-width: 1000px) {
+/* Heading one */
+.fi-heading-one,
+.flutter-interact .fi-landing-hero h3 {
+font-size: 64px;
+}
+
+/* Heading two */
+.fi-heading-two,
+.flutter-interact .fi-section--hero h2,
+.flutter-interact .fi-get-involved h2 {
+font-size: 56px;
+}
+
+/* Heading description */
+.fi-heading-description,
+.flutter-interact .fi-section--hero .devsite-landing-row-description,
+.flutter-interact .fi-landing-hero .devsite-landing-row-item-description-content {
+font-size: 24px;
+width: 70%;
+}
+
+.flutter-interact .fi-section {
+padding: 80px 0 80px;
+}
+
+.flutter-interact .fi-attending-header h2 {
+width: 60%;
+}
+
+.flutter-interact .fi-schedule-table h3 {
+font-size: 32px;
+line-height: 40px;
+}
+}
+</style>
+
+<div class="devsite-article-body clearfix
+devsite-no-page-title
+">
+
+<section class="devsite-landing-row devsite-landing-row-1-up fi-section fi-section--hero fi-attending-header"
+>
+
+<header class="devsite-landing-row-header">
+<div class="devsite-landing-row-header-text">
+
+<h2 id="flutter-interact-is-coming-to-bklyn-studios-on-wednesday-december-11-2019">
+Flutter Interact is coming to DSC MESCOE on Saturday,<br/> December 14, 2019.
+</h2>
+
+<div class="devsite-landing-row-description">
+Get hands on with industry peers. Workshop, experiment and share experiences building beautiful apps with Flutter.
+
+</div>
+</div>
+</header>
+</section>
+
+<section class="devsite-landing-row devsite-landing-row-1-up fi-section devsite-landing-row-100"
+background="white"
+>
+
+<div class="devsite-landing-row-group">
+<div class="devsite-landing-row-item devsite-landing-row-item-no-media"
+>
+
+
+<div class="devsite-landing-row-item-description"
+>
+
+
+<div class="devsite-landing-row-item-body">
+
+<div class="devsite-landing-row-item-description-content">
+<div class="devsite-event-map">
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.87189276813!2d73.87809991486465!3d18.534690387400747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c19b6e6e8b35%3A0x8cab210bf925c00e!2sDSC%20MESCOE!5e0!3m2!1sen!2sin!4v1573667393877!5m2!1sen!2sin" width="100%" height="700px" frameborder="0"  allowfullscreen=""></iframe>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</section>
+<section class="devsite-landing-row devsite-landing-row-2-up fi-section"
+background="white"
+>
+
+<div class="devsite-landing-row-group">
+<div class="devsite-landing-row-item devsite-landing-row-item-no-media fi-col fi-col--30"
+>
+
+
+<div class="devsite-landing-row-item-description"
+>
+
+
+<div class="devsite-landing-row-item-body">
+<h3 id="directions">
+Directions</h3>
+
+</div>
+</div>
+</div>
+<div class="devsite-landing-row-item devsite-landing-row-item-no-media fi-col fi-col--70 fi-attending-col"
+>
+
+
+<div class="devsite-landing-row-item-description"
+>
+
+
+<div class="devsite-landing-row-item-body">
+
+<div class="devsite-landing-row-item-description-content">
+<h4>DSC MESCOE Event Space</h4> <p>19, Late Prin. V.K. Joag Path, Wadia College Campus,<br/>Pune, Maharashtra 411001<br/> <a href="https://www.dscmescoe.com/" rel="noopener noreferrer" target="_blank">Visit DSC MESCOE website</a></p>
+<!--
+<h4>Subway</h4> <ul>
+<li>DeKalb Avenue Station (on-site), lines B, D, N, Q, and R</li>
+<li>Hoyt Street Station, lines 2 and 3</li>
+<li>Nevins Street, lines 4 and 5</li>
+<li>Hoyt-Schermerhorn Street, lines A, C and G</li>
+-->
+</ul> <h4>Parking in DSC MESCOE</h4> <ul>
+<li>Take right to main gate and park there</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+</article>
+</article>
+</devsite-content>
+</div>
+</div></div>
+<devsite-sitemask></devsite-sitemask>
+<devsite-snackbar ></devsite-snackbar> <devsite-tooltip ></devsite-tooltip>
+<devsite-analytics>
+<script type="application/json" analytics>[{"metrics": {"ratings_value": "metric1", "ratings_count": "metric2"}, "dimensions": {"dimension6": "en", "dimension4": "Flutter Interact", "dimension5": "en", "dimension3": false, "dimension1": "Signed In", "dimension8": null}, "gaid": "UA-24532603-1"}]</script>
+<script type="application/json" gtm>[]</script>
+</devsite-analytics>
+<script>
+(function(d,e,v,s,i,t,E){d['GoogleDevelopersObject']=i;
+t=e.createElement(v);t.async=1;t.src=s;E=e.getElementsByTagName(v)[0];
+E.parentNode.insertBefore(t,E);})(window, document, 'script',
+'https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/js/app_loader.js', '[1,"en",null,"/js/devsite_app.js","https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1","https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers","https://developers-dot-devsite-v2-prod.appspot.com",1,null,["/_pwa/developers/manifest.json","/_static/images/video-placeholder.svg","https://www.gstatic.com/devrel-devsite/prod/v98dba58d370055a75e2a5d5d215c4ecfda28b813705f75c7e2758703570175e1/developers/images/favicon.png","https://fonts.googleapis.com/css?family=Google+Sans:400,500|Roboto:400,400italic,500,500italic,700,700italic|Roboto+Mono:400,500,700|Material+Icons"]]')
+</script> </body>
+</html>
